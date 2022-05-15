235691bd4243c0689e3051147b53abdf362fb140
Aleksii <__>
2022-05-15 18:25:13 +0300

    added finction hello user name

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ def say() -> str:
     return "Hello world"
 
 
+def hello_name(name: str = "User") -> str:
+    """Hello.
+
+    Args:
+        name (str, optional): user name. Defaults to "User".
+
+    Returns:
+        str: Hello {user name}
+    """
+    return f"Hello {name}"
+
+
 if __name__ == "__main__":
     print(say())
-
+    name = input("What is your name? - ")
+    print(hello_name(name))
