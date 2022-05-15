7465ab3e8668277eedce89b412f88f4fdd373c20
Aleksii <__>
2022-05-15 18:06:37 +0300

    added func say in main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
+def say() -> str:
+    """tst function.
+
+    Returns:
+        str: Hello world
+    """
+    return "Hello world"
+
 
 if __name__ == "__main__":
-    print("Hello world")
+    print(say())
 
